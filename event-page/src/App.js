d86b7c2148efc46d000b8e11fdab8b92dc3923e0
Karel Rojas Requena <karelrojas64@gmail.com>
2024-05-09 15:25:38 -0400

    event-page retrieves list of members based on selection

--- event-page/src/App.js
+++ event-page/src/App.js
@@ -1,35 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [members, setMembers] = useState([]);
 
-  const getGuard = () => {
-    axios.get('http://localhost:8081/event/members/guards')
-      .then(res => {
-        console.log(res.data);
-      }).catch(err => {
-        console.log(err);
-      })
-  }
+  // useEffect(() => {
+  //   getAttendees();
+  // }, []);
 
-  const getAttendee = () => {
-    axios.get('http://localhost:8081/event/members/attendees')
-    .then(res => {
-      console.log(res.data);
-    }).catch(err => {
-      console.log(err);
-    })
+  // Sends and recieves GET request from selection
+  async function getMembers(event) {
+    const value = event.target.value;
+    const res = await axios.get('http://localhost:8081/event/members/' + value);
+    setMembers(res.data);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>Test</p>
-        <div>Example</div>
-        <button onClick={getGuard} className="btn">Guard Info</button>
-        <button onClick={getAttendee} className="btn">Attendee Info</button>
+          <select className="member-selection" onChange={getMembers}>
+            <option value="attendees">Attendees</option>
+            <option value="vendors">Vendors</option>
+            <option value="guards">Guards</option>
+            <option value="officers">Officers</option>
+          </select>
+        <ul className="members-list">
+          {members.map((data) => {
+            return (
+              <li className="members-list-item" key={data.id}>{data.id} {data.firstName} {data.lastName}</li>
+            )
+          })}
+        </ul>
       </header>
     </div>
   );
